4bf5a688b1ee43276d5c3bf383bfa5684c92bd7c
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-07-04 16:58:08 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -6,6 +6,12 @@ public class reverse {
 	 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 	 System.out.println("Enter the String");
 	  s=br.readLine();
+	 if(s.length()<=1)
+	  {
+		  System.out.println(s);
+	  }
+	  else
+	  {
 	 StringBuilder b=new StringBuilder(s);
 	 b=b.reverse();
 	 System.out.println(b);
