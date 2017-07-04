7adb142f3ba1753a77453705d1d67d42bdd4cf61
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-07-04 17:03:35 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -6,7 +6,7 @@ public static void main(String args[])
     int i,fact=1;
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter the number:");
-    int n=sc.nextInt;
+    int n=sc.nextInt();
     for(i=1;i<n;i++)
 {
 fact=fact*i;
