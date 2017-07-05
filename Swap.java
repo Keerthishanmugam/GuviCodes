6fe1e0be2409ed71251c2da137ce67cfbbcc84e6
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-07-05 10:29:44 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,30 @@
+
+
+import java.io.*;
+
+
+public class Swap {
+public static void main(String args[])throws IOException
+{
+	int i;
+	char temp=' ';
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	//StringBuffer b=new StringBuffer(b);
+	
+	//char[] c2=new char[50];
+	System.out.println("Enter the String");
+	String s=br.readLine();
+	char[] c=s.toCharArray();
+	int l=s.length();
+	
+	for(i=0;i<l-1;i++)
+	{
+	temp=c[i];
+	c[i]=c[i+1];
+	c[i+1]=temp;
+	i++;
+	 }
+	String s1=new String(c);
+	System.out.println(s1);
+}
+}
