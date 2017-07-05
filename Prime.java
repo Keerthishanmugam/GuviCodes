6a35d6a0e6f520c8a5567a75e3dccd21d9efe1f1
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-07-05 14:50:40 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,46 +1,43 @@
-
 import java.io.*;
 import java.util.*;
-
-public class Prime
-public static void main(String args[])throws IOException
-    {
-        int s,e,i=0,j=0;
-        int count=0,flag=0;
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-      
-        List<Integer>list=new ArrayList<Integer>();
-        
-       
-      
-        System.out.println("Enter the starting number:");
-        s=Integer.parseInt(br.readLine());
-        System.out.println("Enter the ending number:");
-        e=Integer.parseInt(br.readLine());
-       for( i=s;i>=s&&i<=e;i++)
-       {
-          
-              int m=i/2;
-             for( j=2;j<m;j++)  
-             {
-                 if(i%j==0)
-                 {
-                     flag=1;
-                     break;
-                 }
-             
-             }
-                if(flag==0)
-             {
-            
-            
-                count++;
-              }
-       
-       }
+public class Main{
+  public static void main(String args[])throws IOException
+  {
+    int s,e,flag=0,i=0,j=0;
+    int count=0;
     
-   
-      System.out.println(count);
-  
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    
+    
+     char[] c=new char[100];
+    
+    
+    System.out.println("Enter the starting number:");
+    s=Integer.parseInt(br.readLine());
+    System.out.println("Enter the ending number:");
+    e=Integer.parseInt(br.readLine());
+    for( i=s;i>=s&&i<=e;i++)
+    {
+      
+      int m=i/2;
+      for( j=2;j<m;j++)  
+      {
+        if(i%j==0)
+        {
+          c[i]=1;
+          break;
+        }
+        
+      }
+      if(c[i]==0)
+      {
+          count++;
+        
+      }
+      
     }
+    
+    System.out.println(count);
+    
+  }
 }
