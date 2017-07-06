cc7f4bae925018a5f0f50da565572ec1ab833462
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-07-06 14:37:50 +0530

    Update Time.java

--- Time.java
+++ Time.java
@@ -1,17 +1,22 @@
-import java.util.*;
+package guvicode;
+import java.io.*;
 public class Time {
-	private static Scanner sc;
+	
 
-	public static void main(String args[])
+	public static void main(String args[])throws IOException
 	{
 		int h1,h2,h,mins;
 		float s,e;
 		double mins1,mins2;
-		sc = new Scanner(System.in);
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter the Starting time");
-		 s=sc.nextFloat();
+		 s=Float.parseFloat(br.readLine());
+		 System.out.println("Enter am or pm");
+		 String m=br.readLine();
 		System.out.println("Enter the Ending time");
-		 e=sc.nextFloat();
+		 e=Float.parseFloat(br.readLine());
+		 System.out.println("Enter am or pm");
+		 String m1=br.readLine();
 		h1=(int)s;
 		h2=(int)e;
 		mins1=(int) ((s)*100%100);
@@ -25,6 +30,7 @@ public class Time {
 		{
 			h2=0;
 		}
+		
 	     if(mins2<mins1)
 	     {
 	    	 mins1=60-mins1;
@@ -40,6 +46,11 @@ public class Time {
 	    	System.out.println(mins);
 	    	h=h2-h1;
 	     }
+	     if(m!=m1)
+	     {
+	    	h=h+12; 
+	     }
+	     
 		System.out.println(h+" hours "+mins+" minutes.");
 	}
 
