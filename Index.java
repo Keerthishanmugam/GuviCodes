e86f24c2c1d7b21541963731e0c65ebd24c5401a
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-07-07 14:19:31 +0530

    Create Index.java

---
+++ Index.java
@@ -0,0 +1,26 @@
+import java.io.*;
+public class Index {
+	public static void main(String args[])throws IOException
+	{
+		int s,i,a;
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the length of the array");
+		int n=Integer.parseInt(br.readLine());
+		int[] n1=new int[n];
+		for(i=0;i<n;i++)
+		{
+			s=Integer.parseInt(br.readLine());
+			n1[i]=s;
+		}
+		for(i=0;i<n;i++)
+		{
+			 a=n1[i];
+		
+		if(a==i)
+		{
+			System.out.println(a);
+		}
+		}
+	}
+
+}
