dc74a69b27f738c34116e8fd774aaf21491130ef
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-07-07 14:28:01 +0530

    Create Unrepeated.java

---
+++ Unrepeated.java
@@ -0,0 +1,48 @@
+package guvicode;
+
+import java.io.*;
+import java.util.*;
+public class Unrepeated {
+	public static void main(String args[])throws IOException
+	{
+		int i,a;
+		String s = null;
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		List<String>list=new ArrayList<String>();
+		Set<String>list1=new HashSet<String>();
+		System.out.println("enter the value of n");
+		int n=Integer.parseInt(br.readLine());
+		System.out.println("Enter the register numbers"); 
+		
+	   for(i=0;i<n;i++)
+	   {
+		    s=br.readLine();
+		   list.add(s);
+		 
+	   }
+	   if(n>1)
+	   {
+	   for(String s1:list)
+	   {
+		   a=Collections.frequency(list,s1);
+		 
+	   if(a<2)
+	   {
+		   list1.add(s1);
+	   }
+	    
+	   }
+	   System.out.println("The unrepeated unique number:");
+		for(String s2:list1)
+		{
+		System.out.println(s2);
+		}
+	   }
+	   else
+	   {
+		   System.out.println(s);
+	   }
+	}
+}
