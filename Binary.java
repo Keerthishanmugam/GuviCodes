7f8962a8d05286fb792e57438e90ac4897803314
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-07-14 14:41:31 +0530

    Create Binary.java

---
+++ Binary.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*; 
+public class Binary {
+	
+public static void main(String args[])throws IOException
+{
+	
+	int n,r,i;
+	
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	List<String>list=new ArrayList<String>();
+	System.out.println("Enter the number");
+	n=Integer.parseInt(br.readLine());
+	r=(int) Math.pow(2,n);
+	
+	for(i=0;i<r;i++)
+	{
+		
+		String b=Integer.toBinaryString(i);
+		StringBuffer s=new StringBuffer(b);
+		while(s.length()<n)
+		{
+			s.insert(0, "0");
+		}
+		String b1=s.toString();
+		list.add(b1);
+		
+		
+	}
+	for(String x:list)
+	{
+		System.out.println(x);
+	}
+	
+}
+}
