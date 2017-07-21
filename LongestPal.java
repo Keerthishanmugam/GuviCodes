30993e3ed44217066d8cb726a8767f25ba6907c9
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-07-21 09:37:02 +0530

    Update and rename Longestpal.java to LongestPal.java

---
+++ LongestPal.java
@@ -0,0 +1,43 @@
+package guvi;
+import java.io.*;
+import java.util.*;
+
+public class LongestPal
+{
+public static void main(String args[])throws IOException
+{
+  int i,occurance,first;
+  String word,rev,sub;
+  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+  List<String>list=new ArrayList<String>();
+  System.out.println("Enter the word");
+  word=br.readLine();
+  
+   
+  for(i=0;i<word.length();i++)
+  {
+   
+    first=word.indexOf(word.charAt(i));
+    occurance=word.indexOf("y",first+1);
+    occurance=word.indexOf(word.charAt(i),first+1);
+    if(!(occurance==-1))
+    {
+    
+     sub=word.substring(first,occurance)+word.charAt(i);
+     StringBuffer b=new StringBuffer(sub);
+      rev=b.reverse().toString();
+      
+      if(sub.equals(rev))
+      {
+        list.add(sub);
+        
+        
+      }
+      
+        }
+      
+  }
+  System.out.println("Palindrome :"+Collections.max(list)); 
+  }
+}
+
