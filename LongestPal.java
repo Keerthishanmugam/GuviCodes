81a41566ee05731e707cb0145797fe112885797f
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-07-21 09:42:21 +0530

    Update LongestPal.java

--- LongestPal.java
+++ LongestPal.java
@@ -1,4 +1,3 @@
-package guvi;
 import java.io.*;
 import java.util.*;
 
@@ -6,38 +5,44 @@ public class LongestPal
 {
 public static void main(String args[])throws IOException
 {
-  int i,occurance,first;
-  String word,rev,sub;
-  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-  List<String>list=new ArrayList<String>();
-  System.out.println("Enter the word");
-  word=br.readLine();
-  
-   
-  for(i=0;i<word.length();i++)
-  {
-   
-    first=word.indexOf(word.charAt(i));
-    occurance=word.indexOf("y",first+1);
-    occurance=word.indexOf(word.charAt(i),first+1);
-    if(!(occurance==-1))
-    {
-    
-     sub=word.substring(first,occurance)+word.charAt(i);
-     StringBuffer b=new StringBuffer(sub);
-      rev=b.reverse().toString();
-      
-      if(sub.equals(rev))
-      {
-        list.add(sub);
-        
-        
-      }
-      
-        }
-      
-  }
-  System.out.println("Palindrome :"+Collections.max(list)); 
-  }
+	int i,occurance,first,flag=0;
+	String word,rev,sub;
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	List<String>list=new ArrayList<String>();
+	System.out.println("Enter the word");
+	word=br.readLine();
+	
+	 
+	for(i=0;i<word.length();i++)
+	{
+	 
+	  first=word.indexOf(word.charAt(i));
+	  occurance=word.indexOf("y",first+1);
+	  occurance=word.indexOf(word.charAt(i),first+1);
+	  if(!(occurance==-1))
+		{
+		
+		 sub=word.substring(first,occurance)+word.charAt(i);
+		 StringBuffer b=new StringBuffer(sub);
+			rev=b.reverse().toString();
+			
+			if(sub.equals(rev))
+			{
+				flag=1;
+				list.add(sub);
+			}
+			
+				}
+			
+	}
+	if(flag==0)
+	{
+		System.out.println("No palindrome present");
+	}
+	else
+	{
+	System.out.println("Palindrome :"+Collections.max(list));	
+	}
+}
 }
 
