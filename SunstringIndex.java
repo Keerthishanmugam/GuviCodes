493948f77bcb747bdfc9e35186c3e9b3fe666cf0
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-07-24 14:43:12 +0530

    Create SunstringIndex.java

---
+++ SunstringIndex.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class SubstringIndex {
+  public static void main(String args[])throws IOException
+  {
+    String word,sub;
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter the String");
+    word=br.readLine();
+    System.out.println("Enter the substring");
+    sub=br.readLine();
+    if(word.contains(sub))
+    {
+        char c=sub.charAt(0);
+        System.out.println(word.indexOf(c));
+    }
+  }
+}
