fb2db82f4ece5b7e7614d8671c2a0b6d19faf817
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-08-01 12:02:07 +0530

    Create MaximumWeigth.java

---
+++ MaximumWeigth.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Arrays;
+public class MaximumWeight {
+public static void main(String args[])throws IOException
+{
+	String values;
+	int elephant,web,i;
+	int weight = 0,count=0;
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Enter the number of elephants and maximum weight");
+	values=br.readLine();
+	String[] value=values.split(" ");
+	elephant=Integer.parseInt(value[0]);
+	web=Integer.parseInt(value[1]);
+	String weights=br.readLine();
+	String[] value1=weights.split(" ");
+	int[]elephants=new int[elephant];
+	for(i=0;i<elephant;i++)
+	{
+		elephants[i]=Integer.parseInt(value1[i]);
+	}
+	Arrays.sort(elephants);
+	for(int x:elephants)
+	{
+		weight=weight+x;
+		count++;
+		if(weight>web)
+		{
+		 count--;
+		 break;
+		}
+	}
+	System.out.println(count);
+}
+}
