6926703eaac79520c19bfe5b8bb8c1f450ac144d
KeerthanaShanmugasundaram <keerthana0227@gmail.com>
2017-08-08 12:15:49 +0530

    Update CoinsCount.java

--- CoinsCount.java
+++ CoinsCount.java
@@ -4,40 +4,40 @@ public class CoinsCount {
 public static void main(String args[])throws IOException
 {
 	String coins;
-	int sum,i,value,NumberOfCoins,total=0;
+	int sum,i,value,NumberOfCoins,total=0,amount=0;
 	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 	List<Integer>list=new ArrayList<Integer>();
+	
 	System.out.println("Enter the coins");
     coins=br.readLine();
     String[] coin=coins.split(",");
     Integer[] coinValue=new Integer[coin.length];
+    Integer[] sum1=new Integer[coin.length];
+    
     for(i=0;i<coin.length;i++)
     {
+    	
      coinValue[i]=Integer.parseInt(coin[i]);
     }
     Arrays.sort(coinValue,Collections.reverseOrder());
+   
     System.out.println("Enter the sum");
     sum=Integer.parseInt(br.readLine());
     for(i=0;i<coin.length;i++)
     {
+    	
     	value=coinValue[i];
-    	NumberOfCoins=sum/value;
-    	if(NumberOfCoins*value==sum)
-    	{
-    		list.add(NumberOfCoins);
-    		break;
-    	}
-    	else
-    	{
-    		list.add(NumberOfCoins);
-    		sum=sum-(NumberOfCoins*value);
-    	}
-    }
-  for(int j:list)
+        NumberOfCoins=sum/value;
+    	amount=amount+NumberOfCoins*value;
+    	list.add(NumberOfCoins);
+    	sum=sum-(NumberOfCoins*value);
+    	sum1[i]=sum;
+    	    }
+    for(int j:list)
   {
 	  total=total+j;
   }
-  System.out.println("Total number of coins: "+total);
+    System.out.println("Total number of coins: "+total);
     for(i=0;i<coinValue.length;i++)
     {
     	if(list.get(i)==1)
@@ -50,6 +50,7 @@ public static void main(String args[])throws IOException
     	}
     	 
     }
+        }
   
 }
-}
+
